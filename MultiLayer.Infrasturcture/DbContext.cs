30c3ce7ad863e6577e4c011e9ffdde9f0cdafa93
oykunnaz <50181974+oykunnaz@users.noreply.github.com>
2019-05-02 19:16:27 +0300

    Add files via upload

---
+++ MultiLayer.Infrasturcture/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MultiLayer.Domain;
+using MultiLayer.Domain.Entities;
+using MultiLayer.Presentation.Models;
+
+namespace MultiLayer.Infrasturcture
+{
+    public class MultilayerDbContext : IdentityDbContext<ApplicationUser>, IDbContext
+    {
+        public MultilayerDbContext()
+           : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public DbSet<Forms> Forms { get; set; }
+        public DbSet<fields> fields { get; set; }
+    }
+}
