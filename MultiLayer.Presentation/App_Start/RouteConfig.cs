80af908058b35b64ccc9e44496aa38df784e560a
oykunnaz <50181974+oykunnaz@users.noreply.github.com>
2019-05-02 19:07:56 +0300

    Add files via upload

---
+++ MultiLayer.Presentation/App_Start/RouteConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MultiLayer.Presentation
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            
+            routes.MapRoute(
+                name: "Forms",
+                url: "forms/{id}",
+                defaults: new { controller = "Forms", action = "forms", id = UrlParameter.Optional },
+                constraints: new { id = @"\d+" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Forms", action = "Index", id = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
---
+++ MultiLayer.Presentation/Controllers/FormsController.cs
@@ -0,0 +1,99 @@
+﻿using MultiLayer.Services.FormServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using MultiLayer.Presentation.Models;
+
+namespace MultiLayer.Presentation.Controllers
+{
+    [Authorize]
+    public class FormsController : Controller
+    {
+        private readonly IFormsService _formService;
+
+        public FormsController(IFormsService formService)
+        {
+            _formService = formService;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_formService.GetForms());
+        }
+
+        public ActionResult forms(int id)
+        {
+            ViewBag.Fields = _formService.GetFields(id).ToList();
+            return View(_formService.Find(id));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Forms model, string fields)
+        {
+            model.createdAt = DateTime.Now;
+            model.createdBy = User.Identity.GetUserId();
+            fields = fields.Remove(fields.Length - 1);
+            try
+            {
+
+                _formService.Insert(model);
+                int id = model.id;
+                if(id > 0)
+                {
+                    string[] fArr = fields.Split('€');
+                    foreach (var fa in fArr)
+                    {
+                        string[] alanlar = fa.Split('#');
+                        fields f = new fields();
+                        f.name = alanlar[1];
+                        f.dataType = alanlar[2];
+                        if(alanlar[3] == "checked")
+                        {
+                            f.required = true;
+                        }else
+                        {
+                            f.required = false;
+                        }
+                        f.formsId = id;
+
+                        _formService.InsertFields(f);
+                    }
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+
+        public ActionResult jsonRes(int id)
+        {
+            return RedirectToAction("getJson", new { id= id});
+        }
+
+        public JsonResult getJson(int id)
+        {
+            jsonR ret = new jsonR();
+            ret.formBilgileri = _formService.Find(id);
+            ret.alanlar = _formService.GetFields(id).ToList();
+
+            return Json(ret, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class jsonR
+    {
+        public Forms formBilgileri { get; set; }
+        public List<fields> alanlar { get; set; }
+    }
+}
